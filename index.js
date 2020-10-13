c60656b192a3f0938355d7d186e89fc97fafc864
ruizaj13 <ruizaj13@gmail.com>
2020-10-13 15:56:43 -0700

    refractoring everything is a mess

--- index.js
+++ index.js
@@ -13,28 +13,29 @@ console.log('its working');
 (c) Home Team goals for 2014 world cup final
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
- 
-console.log(fifaData.filter(game => game['Year'] === 2014 && game['Stage'] === 'Final')[0]['Home Team Name']);
-console.log(fifaData.filter(game => game['Year'] === 2014 && game['Stage'] === 'Final')[0]['Away Team Name']);
-console.log(fifaData.filter(game => game['Year'] === 2014 && game['Stage'] === 'Final')[0]['Home Team Goals']);
-console.log(fifaData.filter(game => game['Year'] === 2014 && game['Stage'] === 'Final')[0]['Away Team Goals']);
-console.log(fifaData.filter(game => game['Year'] === 2014 && game['Stage'] === 'Final')[0]['Win conditions']);
+ console.log(fifaData[828]['Home Team Name'])
+ console.log(fifaData[828]['Away Team Name'])
+ console.log(fifaData[828]['Home Team Goals'])
+ console.log(fifaData[828]['Away Team Goals'])
+ console.log(fifaData[828]['Win conditions'])
+
+
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
+function getFinals(data) {
+    data.filter(data => data.Stage === 'Final')
+}
 
-let finalsData = fifaData.filter(data => data.Stage === 'Final')
+console.log(getFinals(fifaData));
 
-    console.log(finalsData)
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
-
-    /* code here */
-
+function getYears(callBack) {
+    
 };
 
-getYears();
+
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
